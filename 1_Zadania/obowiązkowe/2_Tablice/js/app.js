2aac401e4db90ba16a3adedf72f1b0cbc3729d6c
Cieśla Łukasz <lukasz.ciesla@microsoft.wsei.edu.pl>
2019-10-06 14:20:34 +0200

    Add next task

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -5,7 +5,7 @@ function distFromAvarage(tab) {
     }
     avg = sum / tab.length;
 
-    var t = new Array();
+    var t = [];
     for (let i = 0; i < tab.length; i++) {
         t.push(tab[i] - avg);
     }
@@ -14,3 +14,54 @@ function distFromAvarage(tab) {
 }
 
 distFromAvarage([1, 2, 3, 4, 5, 6, 7]);
+
+//--------------------------------------------
+// task 2
+var fruits = [
+    'Banan',
+    'Jabłko',
+    'Gruszka',
+    'Śliwka',
+    'Borowka'
+];
+
+console.log(fruits[0]);
+console.log(fruits[fruits.length-1]);
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+//--------------------------------------------
+// task 3
+function printTable(tab) {
+    for (let i = 0; i < tab.length; i++) {
+        console.log(tab[i]);
+    }
+}
+
+printTable([1, 3, 5, 7, 9]);
+
+//---------------------------------------------
+// task 4
+function multiply(tab) {
+    var m = 1;
+    for (let i = 0; i < tab.length; i++) {
+        m *= tab[i];
+    }
+    return m;
+}
+
+multiply([1,2,3,4,5,6,7]);
+
+function getEvenAvarage(tab) {
+    var sum = 0, counter = 0;
+    for (let i = 0; i < tab.length; i++) {
+        if (!(tab[i] % 2)) {
+            sum += tab[i];
+            counter++;
+        }
+    }
+
+    return counter > 0 ? sum/counter : null;
+}
+getEvenAvarage([1,1,1,1]);
