ab591e4651cfa72d0a4bdda5bde89a216f95ae0e
Cieśla Łukasz <lukasz.ciesla@microsoft.wsei.edu.pl>
2019-10-06 13:59:59 +0200

    Complete

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -1,4 +1,5 @@
 
+/*
 var task1Array = [
     [2, 3],
     ["Ala", "Ola"],
@@ -13,3 +14,31 @@ var task2Array = [
     [5, 6, 7, 8],
     [9, 10, 11, 12]
 ];
+
+*/
+var arr = [
+    [11, 12],
+    [42, 2],
+    [-4, -120],
+    [0, 0],
+    [1, 34]
+];
+
+function checkArray(tab) {
+    var t = [];
+    for (let i = 0; i < tab.length; i++) {
+        t[i] = [];
+    }
+
+    for (let y = 0; y < tab.length; y++) {
+        for (let x = 0; x < tab[y].length; x++) {
+            if (!(tab[y][x] % 2)) {
+                t[y][x] = true;
+            }
+            else t[y][x] = false;
+        }
+    }
+    return t;
+}
+
+checkArray(arr);
